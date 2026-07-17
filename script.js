5c3ed4b40d0efc67e4e490dd983fbc99ff97eed8
TecnoRick <ricardoestebanbejarano@gmail.com>
2026-07-16 23:38:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Scroll suave para los enlaces del menú
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
